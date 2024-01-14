737601b07f363696ce7fe33fa789dfcc4660492a
BetaTester41 <76068161+BetaTester41@users.noreply.github.com>
2024-01-13 23:10:36 -0800

    Port to 1.20.1

--- src/main/java/momo/grounded_origins/registry/ModItems.java
+++ src/main/java/momo/grounded_origins/registry/ModItems.java
@@ -4,9 +4,11 @@ import momo.grounded_origins.GroundedOrigins;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 
 
 public class ModItems {
@@ -18,17 +20,19 @@ public class ModItems {
     public static final Item PENGUIN_ICON = new Item(new FabricItemSettings());
 
     // Moth food
-    public static final Item FLOWER_STAMENS = new Item(new FabricItemSettings().group(ItemGroup.FOOD)
-            .food(new FoodComponent.Builder().alwaysEdible().build())
+    public static final Item FLOWER_STAMENS = new Item(new FabricItemSettings().food(new FoodComponent.Builder().alwaysEdible().build())
     );
 
     public static void init() {
         // Icons
-        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "beach_crab_icon"), BEACH_CRAB_ICON);
-        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "deer_icon"), DEER_ICON);
-        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "moth_icon"), MOTH_ICON);
-        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "penguin_icon"), PENGUIN_ICON);
+        Registry.register(Registries.ITEM, new Identifier(GroundedOrigins.MOD_ID, "beach_crab_icon"), BEACH_CRAB_ICON);
+        Registry.register(Registries.ITEM, new Identifier(GroundedOrigins.MOD_ID, "deer_icon"), DEER_ICON);
+        Registry.register(Registries.ITEM, new Identifier(GroundedOrigins.MOD_ID, "moth_icon"), MOTH_ICON);
+        Registry.register(Registries.ITEM, new Identifier(GroundedOrigins.MOD_ID, "penguin_icon"), PENGUIN_ICON);
         // Moth food
-        Registry.register(Registry.ITEM, new Identifier(GroundedOrigins.MOD_ID, "flower_stamens"), FLOWER_STAMENS);
+        Registry.register(Registries.ITEM, new Identifier(GroundedOrigins.MOD_ID, "flower_stamens"), FLOWER_STAMENS);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entry -> {
+            entry.add(FLOWER_STAMENS);
+        });
     }
 }
